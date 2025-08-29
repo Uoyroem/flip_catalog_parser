066c10b499b38fc8608971e63cddecf52ed99614
Ahatron <ahatpolatov@gmail.com>
2025-08-29 12:05:14 +0000

    FIX test

--- tests/test_catalog_parse.py
+++ tests/test_catalog_parse.py
@@ -5,23 +5,24 @@ BASE_URL = "http://localhost:8000/api"
 
 
 def test_parse_and_get_products():
-
-    post_url = f"{BASE_URL}/catalogs/parse-products/by-code/2649"
+    post_url = f"{BASE_URL}/catalogs/parse-products/by-url"
     print(f"\nОтправка POST-запроса на: {post_url}")
 
     try:
-        post_response = requests.post(post_url)
+        post_response = requests.post(
+            post_url, params={"url": "https://www.flip.kz/catalog?subsection=2649"}
+        )
         post_response.raise_for_status()
         print(f"Статус ответа POST: {post_response.status_code}")
 
     except requests.exceptions.RequestException as e:
         pytest.fail(f"Ошибка при выполнении POST-запроса: {e}")
 
-    get_url = f"{BASE_URL}/products?limit=10"
+    get_url = f"{BASE_URL}/products"
     print(f"\nОтправка GET-запроса на: {get_url}")
 
     try:
-        get_response = requests.get(get_url)
+        get_response = requests.get(get_url, params={"limit": 10})
         get_response.raise_for_status()
         products = get_response.json()
 
